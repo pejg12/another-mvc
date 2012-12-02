1a53e6dfef0ddc11d086829080933fac018076f8
pejg12 <pejg12@student.bth.se>
2012-12-02 12:37:06 +0100

    renamed Lydia to Another MVC, including internal variables

---
+++ src/CAmvc/CAmvc.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+* Main class for Another MVC, holds everything.
+*
+* @package AnotherMVCCore
+*/
+class CAmvc implements ISingleton {
+
+   private static $instance = null;
+
+   /**
+    * Singleton pattern. Get the instance of the latest created object or create a new one.
+    * @return CAmvc The instance of this class.
+    */
+   public static function Instance() {
+      if(self::$instance == null) {
+         self::$instance = new CAmvc();
+      }
+      return self::$instance;
+   }
+
+   /**
+    * Constructor
+    */
+   protected function __construct() {
+      // include the site specific config.php and create a ref to $amvc to be used by config.php
+      $amvc = &$this;
+      require(AMVC_SITE_PATH.'/config.php');
+   }
+
+  /**
+   * Frontcontroller, check url and route to controllers.
+   */
+  public function FrontControllerRoute() {
+    // Take current url and divide it in controller, method and parameters
+    $this->request = new CRequest($this->config['url_type']);
+    $this->request->Init($this->config['base_url']);
+    $controller = $this->request->controller;
+    $method     = $this->request->method;
+    $arguments  = $this->request->arguments;
+
+    // Is the controller enabled in config.php?
+    $controllerExists    = isset($this->config['controllers'][$controller]);
+    $controllerEnabled    = false;
+    $className             = false;
+    $classExists           = false;
+
+    if($controllerExists) {
+      $controllerEnabled    = ($this->config['controllers'][$controller]['enabled'] == true);
+      $className            = $this->config['controllers'][$controller]['class'];
+      $classExists          = class_exists($className);
+    }
+
+	// Check if controller has a callable method in the controller class, if then call it
+	if($controllerExists && $controllerEnabled && $classExists) {
+		$rc = new ReflectionClass($className);
+		if($rc->implementsInterface('IController')) {
+			if($rc->hasMethod($method)) {
+				$controllerObj = $rc->newInstance();
+				$methodObj = $rc->getMethod($method);
+				$methodObj->invokeArgs($controllerObj, $arguments);
+			} else {
+				die("404. " . get_class() . ' error: Controller does not contain method.');
+			}
+		} else {
+			die('404. ' . get_class() . ' error: Controller does not implement interface IController.');
+		}
+	}
+	else {
+		die("404. Page is not found. Controller: {$controller}. Method: {$method}. Arguments: " . print_r($arguments, TRUE));
+	}
+
+  }
+
+   /**
+    * ThemeEngineRender, renders the reply of the request.
+    */
+  public function ThemeEngineRender() {
+    // Get the paths and settings for the theme
+    $themeName    = $this->config['theme']['name'];
+    $themePath    = AMVC_INSTALL_PATH . "/themes/{$themeName}";
+    $themeUrl     = $this->request->base_url . "themes/{$themeName}";
+   
+    // Add stylesheet path to the $amvc->data array
+    $this->data['stylesheet'] = "{$themeUrl}/style.css";
+
+    // Include the global functions.php and the functions.php that are part of the theme
+    $amvc = &$this;
+    include(AMVC_INSTALL_PATH . '/themes/functions.php');
+    $functionsPath = "{$themePath}/functions.php";
+    if(is_file($functionsPath)) {
+      include $functionsPath;
+    }
+
+    // Extract $amvc->data to own variables and handover to the template file
+    extract($this->data);     
+    include("{$themePath}/default.tpl.php");
+  }
+
+} // end of class
