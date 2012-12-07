d24aa1af908dcc4837de5693e134326c4d7475df
pejg12 <pejg12@student.bth.se>
2012-12-07 10:52:58 +0100

    guestbook uses SQLite instead of session

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -22,7 +22,7 @@ class CCGuestbook extends CObject implements IController {
    * Implementing interface IController. All controllers must have an index action.
    */
   public function Index() {   
-    $formAction = $this->request->CreateUrl('guestbook/add');
+    $formAction = $this->request->CreateUrl('guestbook/handler');
     $this->pageForm = <<<EOD
       <form action='{$formAction}' method='post'>
         <p>
@@ -32,6 +32,7 @@ class CCGuestbook extends CObject implements IController {
         <p>
           <input type='submit' name='doAdd' value='Add message' />
           <input type='submit' name='doClear' value='Clear all messages' />
+          <input type='submit' name='doCreate' value='Init database' />
         </p>
       </form>
 
@@ -39,31 +40,67 @@ EOD;
     $this->data['title'] = $this->pageTitle;
     $this->data['main']  = $this->pageHeader . $this->pageForm;
    
-    if(isset($_SESSION['guestbook'])) {
-      foreach($_SESSION['guestbook'] as $val) {
-        $this->data['main'] .= "<div style='background-color:#f6f6f6;border:1px solid #ccc;margin-bottom:1em;padding:1em;'><p>At: {$val['time']}</p><p>{$val['entry']}</p></div>\n";
-      }
+    foreach($this->ReadAllFromDatabase() AS $val) {
+      $this->data['main'] .= "<div style='background-color:#f6f6f6;border:1px solid #ccc;margin-bottom:1em;padding:1em;'><p>At: {$val['created']}</p><p>{$val['entry']}</p></div>\n";
     }
   } 
 
   /**
-   * Add a entry to the guestbook.
+   * Read all entries from the database.
    */
-  public function Add() {
+  private function ReadAllFromDatabase() {
+    try {
+      $db = new PDO($this->config['database'][0]['dsn']);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+      $stmt = $db->prepare('SELECT * FROM mvckm3_Guestbook ORDER BY created DESC;');
+      $stmt->execute();
+      $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      return $res;
+    } catch(Exception $e) {
+      return array();
+    }
+  }
+
+  /**
+   * Handle posts from the form and take appropriate action.
+   */
+  public function Handler() {
     if(isset($_POST['doAdd'])) {
-      $entry = strip_tags($_POST['newEntry']);
-      $time = date('Y-m-d H:i:s');
-
-      // it has to be an array for array_unshift() to work
-      $_SESSION['guestbook'] = (is_array($_SESSION['guestbook']) ? $_SESSION['guestbook'] : array());
-
-      // this will put all new messages in the beginning of the array instead of at the end. newest on top :)
-      array_unshift($_SESSION['guestbook'], array('time'=>$time, 'entry'=>$entry));
+      $this->SaveNewToDatabase(strip_tags($_POST['newEntry']));
     }
     elseif(isset($_POST['doClear'])) {
-      unset($_SESSION['guestbook']);
+      $this->DeleteAllFromDatabase();
+    }           
+    elseif(isset($_POST['doCreate'])) {
+      $this->CreateTableInDatabase();
     }           
     header('Location: ' . $this->request->CreateUrl('guestbook'));
+  }
+
+  /**
+   * Save a new entry to database.
+   */
+  private function SaveNewToDatabase($entry) {
+    $db = new PDO($this->config['database'][0]['dsn']);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+    $stmt = $db->prepare('INSERT INTO mvckm3_Guestbook (entry) VALUES (?)');
+    $stmt->execute(array($entry));
+    if($stmt->rowCount() != 1) {
+      die('Failed to insert new guestbook item into database.');
+    }
+  }
+
+  /**
+   * Delete all entries from the database.
+   */
+  private function DeleteAllFromDatabase() {
+    $db = new PDO($this->config['database'][0]['dsn']);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+    $stmt = $db->prepare('DELETE FROM mvckm3_Guestbook');
+    $stmt->execute();
   }
 
   /**
