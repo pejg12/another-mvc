3a23b1833ff50538a0be995273a89290d5e51553
pejg12 <pejg12@student.bth.se>
2012-12-07 16:27:22 +0100

    introduced custom exception handler

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -26,3 +26,11 @@ spl_autoload_register('autoload');
 function htmlent($str, $flags = ENT_COMPAT) {
   return htmlentities($str, $flags, CAmvc::Instance()->config['character_encoding']);
 }
+
+/**
+* Set a default exception handler and enable logging in it.
+*/
+function exception_handler($exception) {
+  echo "Another MVC: Uncaught exception: <p>" . $e->getMessage() . "</p><pre>" . $e->getTraceAsString(), "</pre>";
+}
+set_exception_handler('exception_handler');
