d2e233d8246cac450645df19eeff4e803319960e
pejg12 <pejg12@student.bth.se>
2012-12-09 10:50:20 +0100

    introduced new model, separated model code from the guestbook controller

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -4,112 +4,48 @@
 *
 * @package AnotherMVCCore
 */
-class CCGuestbook extends CObject implements IController, IHasSQL {
+class CCGuestbook extends CObject implements IController {
 
-  private $pageTitle  = 'Another MVC Guestbook Example';
-  private $pageHeader = 'Showing off how to implement a guestbook in Another MVC.';
-  private $pageForm; 
+  private $model;
+
 
   /**
    * Constructor
    */
   public function __construct() {
     parent::__construct();
+    $this->model = new CMGuestbook();
   }
  
 
   /**
    * Implementing interface IController. All controllers must have an index action.
+   * Show a standard frontpage for the guestbook.
    */
   public function Index() {
-    $this->views->SetTitle($this->pageTitle);
+    $this->views->SetTitle('Another MVC Guestbook Example');
     $this->views->AddInclude(__DIR__ . '/index.tpl.php', array(
-      'entries'=>$this->ReadAllFromDatabase(),
-      'formAction'=>$this->request->CreateUrl('guestbook/handler'),
-      'header'=>$this->pageHeader
+      'entries'    => $this->model->ReadAll(),
+      'formAction' => $this->request->CreateUrl('', 'handler'),
+      'header'     => 'Showing off how to implement a guestbook in Another MVC.',
     ));
   }
 
-   /**
-    * Implementing interface IHasSQL. Encapsulate all SQL used by this class.
-    *
-    * @param string $key the string that is the key of the wanted SQL-entry in the array.
-    */
-  public static function SQL($key=null) {
-     $table_gb = 'mvckm3_Guestbook';
-     $queries = array(
-        'create table guestbook'  => "
-          CREATE TABLE IF NOT EXISTS {$table_gb} (
-            id INTEGER PRIMARY KEY, 
-            entry TEXT, 
-            created DATETIME default (datetime('now'))
-          );",
-        'insert into guestbook'   => "INSERT INTO {$table_gb} (entry) VALUES (?);",
-        'select * from guestbook' => "SELECT * FROM {$table_gb} ORDER BY created DESC;",
-        'delete from guestbook'   => "DELETE FROM {$table_gb};",
-     );
-     if(!isset($queries[$key])) {
-        throw new Exception("No such SQL query, key '$key' was not found.");
-      }
-      return $queries[$key];
-   }
-
-  /**
-   * Read all entries from the database.
-   */
-  private function ReadAllFromDatabase() {
-    try {
-      return $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('select * from guestbook'));
-    } catch(Exception $e) {
-      return array();
-    }
-  }
 
   /**
    * Handle posts from the form and take appropriate action.
    */
   public function Handler() {
     if(isset($_POST['doAdd'])) {
-      $this->SaveNewToDatabase(strip_tags($_POST['newEntry']));
+      $this->model->Add(strip_tags($_POST['newEntry']));
     }
     elseif(isset($_POST['doClear'])) {
-      $this->DeleteAllFromDatabase();
+      $this->model->DeleteAll();
     }           
     elseif(isset($_POST['doCreate'])) {
-      $this->CreateTableInDatabase();
+      $this->model->Init();
     }
     $this->RedirectTo($this->request->CreateUrl($this->request->controller));
-  }
-
-  /**
-   * Save a new entry to database.
-   */
-  private function SaveNewToDatabase($entry) {
-    $this->db->ExecuteQuery(self::SQL('insert into guestbook'), array($entry));
-    $this->session->AddMessage('success', 'Successfully inserted new message.');
-    if($this->db->rowCount() != 1) {
-      die('Failed to insert new guestbook item into database.');
-    }
-  }
-
-  /**
-   * Delete all entries from the database.
-   */
-  private function DeleteAllFromDatabase() {
-    $this->db->ExecuteQuery(self::SQL('delete from guestbook'));
-    $this->session->AddMessage('info', 'Removed all messages from the database table.');
-  }
-
-  /**
-    * Create a new database table.
-    */
-  private function CreateTableInDatabase() {
-    try {
-      $this->db->ExecuteQuery(self::SQL('create table guestbook'));
-      $this->session->AddMessage('info', 'Successfully created the database tables (or left them untouched if they already existed).');
-    } catch(Exception$e) {
-      die("Failed to open database: " . $this->config['database'][0]['dsn'] . "</br>" . $e);
-    }
   }
 
 }
