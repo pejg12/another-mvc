329370f84d2aaaca0fb60d4a8f4c826be1057c9a
pejg12 <pejg12@student.bth.se>
2012-12-10 18:35:28 +0100

    index automatically lists all controller/method combinations available using reflection api

--- src/CCIndex/CCIndex.php
+++ src/CCIndex/CCIndex.php
@@ -18,7 +18,27 @@ class CCIndex extends CObject implements IController {
     */
   public function Index() {
     $this->views->SetTitle('The Index Controller');
-    $this->views->AddInclude(__DIR__ . '/index.tpl.php');
+    $this->views->AddInclude(__DIR__ . '/index.tpl.php', array('menu'=>$this->Menu()));
+  }
+
+  /**
+    * A menu that shows all available controllers/methods
+    */
+  private function Menu() {
+    $items = array();
+    foreach($this->config['controllers'] as $key => $val) {
+      if($val['enabled']) {
+        $rc = new ReflectionClass($val['class']);
+        $items[] = $key;
+        $methods = $rc->getMethods(ReflectionMethod::IS_PUBLIC);
+        foreach($methods as $method) {
+          if($method->name != '__construct' && $method->name != '__destruct' && $method->name != 'Index') {
+            $items[] = "$key/" . mb_strtolower($method->name);
+          }
+        }
+      }
+    }
+    return $items;
   }
 
 } 
--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -1,7 +1,7 @@
+<p>This is what you can do for now.</p>
+
 <ul>
-  <li><a href='<?=create_url('index')?>'>Index</a> (this page)</li>
-  <li>Try out different link types if you're the <a href='<?=create_url('developer')?>'>Developer</a>.</li>
-  <li>Check out the <a href='<?=create_url('guestbook')?>'>Guestbook</a></li>
-  <li>Are you a <a href='<?=create_url('user')?>'>User</a>?</li>
-  <li>... or the <a href='../source.php'>source code</a></li>
+<?php foreach($menu as $val): ?>
+<li><a href='<?=create_url($val)?>'><?=$val?></a></li>
+<?php endforeach; ?>
 </ul>
