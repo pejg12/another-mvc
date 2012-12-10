213f382bafe52a328bd86a978967fc8ae7a42b9f
pejg12 <pejg12@student.bth.se>
2012-12-10 17:25:52 +0100

    added login link for user doe

--- src/CCUser/index.tpl.php
+++ src/CCUser/index.tpl.php
@@ -5,6 +5,7 @@
   <li><a href='<?=create_url(null, 'login', 'admin/root')?>'>Login as admin:root (should fail, wrong acronym)</a>
   <li><a href='<?=create_url(null, 'login', 'root/admin')?>'>Login as admin:root (should fail, wrong password)</a>
   <li><a href='<?=create_url(null, 'login', 'admin@dbwebb.se/root')?>'>Login as admin@dbwebb.se:root (should fail, wrong email)</a>
+  <li><a href='<?=create_url(null, 'login', 'doe/doe')?>'>Login as doe:doe (should work)</a>
   <li><a href='<?=create_url(null, 'logout')?>'>Logout</a>
 </ul>
 <p>This is what is known about the current user.</p>
