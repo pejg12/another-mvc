c463386d74039b2939230bc6a80aa2b1555fd169
pejg12 <pejg12@student.bth.se>
2012-12-17 11:29:27 +0100

    moved helper functions

--- src/CAmvc/CAmvc.php
+++ src/CAmvc/CAmvc.php
@@ -108,8 +108,8 @@ class CAmvc implements ISingleton {
       } else {
         die('404. ' . get_class() . ' error: Controller does not implement interface IController.');
       }
-    } 
-    else { 
+    }
+    else {
       die('404. Page is not found.');
     }
 
@@ -121,7 +121,7 @@ class CAmvc implements ISingleton {
   public function ThemeEngineRender() {
     // Save to session before output anything
     $this->session->StoreInSession();
-  
+
     // Is theme enabled?
     if(!isset($this->config['theme'])) {
       return;
@@ -131,7 +131,7 @@ class CAmvc implements ISingleton {
     $themeName    = $this->config['theme']['name'];
     $themePath    = AMVC_INSTALL_PATH . "/themes/{$themeName}";
     $themeUrl     = $this->request->base_url . "themes/{$themeName}";
-    
+
     // Add stylesheet path to the $amvc->data array
     $this->data['stylesheet'] = $themeUrl . "/" . $this->config['theme']['stylesheet'];
 
@@ -151,6 +151,78 @@ class CAmvc implements ISingleton {
     }
     $templateFile = (isset($this->config['theme']['template_file'])) ? $this->config['theme']['template_file'] : 'default.tpl.php';
     include("{$themePath}/{$templateFile}");
+  }
+
+
+  /**
+   * Redirect to another url and store the session
+   */
+  protected function RedirectTo($urlOrController=null, $method=null, $arguments=null) {
+    $amvc = CAmvc::Instance();
+    if(isset($amvc->config['debug']['db-num-queries']) && $amvc->config['debug']['db-num-queries'] && isset($amvc->db)) {
+      $this->session->SetFlash('database_numQueries', $this->db->GetNumQueries());
+    }
+    if(isset($amvc->config['debug']['db-queries']) && $amvc->config['debug']['db-queries'] && isset($amvc->db)) {
+      $this->session->SetFlash('database_queries', $this->db->GetQueries());
+    }
+    if(isset($amvc->config['debug']['timer']) && $amvc->config['debug']['timer']) {
+      $this->session->SetFlash('timer', $amvc->timer);
+    }
+    $this->session->StoreInSession();
+    header('Location: ' . $this->request->CreateUrl($urlOrController, $method, $arguments));
+  }
+
+
+  /**
+   * Redirect to a method within the current controller. Defaults to index-method. Uses RedirectTo().
+   *
+   * @param string method name the method, default is index method.
+   */
+  protected function RedirectToController($method=null, $arguments=null) {
+    $this->RedirectTo($this->request->controller, $method, $arguments);
+  }
+
+
+  /**
+   * Redirect to a controller and method. Uses RedirectTo().
+   *
+   * @param string controller name the controller or null for current controller.
+   * @param string method name the method, default is current method.
+   */
+  protected function RedirectToControllerMethod($controller=null, $method=null, $arguments=null) {
+    $controller = is_null($controller) ? $this->request->controller : null;
+    $method = is_null($method) ? $this->request->method : null;
+    $this->RedirectTo($this->request->CreateUrl($controller, $method, $arguments));
+  }
+
+
+  /**
+   * Save a message in the session. Uses $this->session->AddMessage()
+   *
+   * @param $type string the type of message, for example: notice, info, success, warning, error.
+   * @param $message string the message.
+   * @param $alternative string the message if the $type is set to false, defaults to null.
+   */
+  protected function AddMessage($type, $message, $alternative=null) {
+    if($type === false) {
+      $type = 'error';
+      $message = $alternative;
+    } else if($type === true) {
+      $type = 'success';
+    }
+    $this->session->AddMessage($type, $message);
+  }
+
+
+  /**
+   * Create an url. Uses $this->request->CreateUrl()
+   *
+   * @param $urlOrController string the relative url or the controller
+   * @param $method string the method to use, $url is then the controller or empty for current
+   * @param $arguments string the extra arguments to send to the method
+   */
+  protected function CreateUrl($urlOrController=null, $method=null, $arguments=null) {
+    $this->request->CreateUrl($urlOrController, $method, $arguments);
   }
 
 } // end of class
--- src/CObject/CObject.php
+++ src/CObject/CObject.php
@@ -6,6 +6,7 @@
 */
 class CObject {
 
+   public $amvc;
    public $config;
    public $request;
    public $data;
@@ -21,6 +22,7 @@ class CObject {
     if(!$amvc) {
       $amvc = CAmvc::Instance();
     }
+    $this->amvc     = &$amvc;
     $this->config   = &$amvc->config;
     $this->request  = &$amvc->request;
     $this->data     = &$amvc->data;
@@ -32,74 +34,42 @@ class CObject {
 
 
   /**
-   * Redirect to another url and store the session
+   * Wrapper for same method in CAmvc.
    */
-  protected function RedirectTo($urlOrController=null, $method=null) {
-    $amvc = CAmvc::Instance();
-    if(isset($amvc->config['debug']['db-num-queries']) && $amvc->config['debug']['db-num-queries'] && isset($amvc->db)) {
-      $this->session->SetFlash('database_numQueries', $this->db->GetNumQueries());
-    }    
-    if(isset($amvc->config['debug']['db-queries']) && $amvc->config['debug']['db-queries'] && isset($amvc->db)) {
-      $this->session->SetFlash('database_queries', $this->db->GetQueries());
-    }    
-    if(isset($amvc->config['debug']['timer']) && $amvc->config['debug']['timer']) {
-      $this->session->SetFlash('timer', $amvc->timer);
-    }    
-    $this->session->StoreInSession();
-    header('Location: ' . $this->request->CreateUrl($urlOrController, $method));
+  protected function RedirectTo($urlOrController=null, $method=null, $arguments=null) {
+    $this->amvc->RedirectTo($urlOrController, $method, $arguments);
   }
 
 
   /**
-   * Redirect to a method within the current controller. Defaults to index-method. Uses RedirectTo().
-   *
-   * @param string method name the method, default is index method.
+   * Wrapper for same method in CAmvc.
    */
-  protected function RedirectToController($method=null) {
-    $this->RedirectTo($this->request->controller, $method);
+  protected function RedirectToController($method=null, $arguments=null) {
+    $this->amvc->RedirectToController($method, $arguments);
   }
 
 
   /**
-   * Redirect to a controller and method. Uses RedirectTo().
-   *
-   * @param string controller name the controller or null for current controller.
-   * @param string method name the method, default is current method.
+   * Wrapper for same method in CAmvc.
    */
-  protected function RedirectToControllerMethod($controller=null, $method=null) {
-    $controller = is_null($controller) ? $this->request->controller : null;
-    $method = is_null($method) ? $this->request->method : null;    
-    $this->RedirectTo($this->request->CreateUrl($controller, $method));
+  protected function RedirectToControllerMethod($controller=null, $method=null, $arguments=null) {
+    $this->amvc->RedirectToControllerMethod($controller, $method, $arguments);
   }
 
 
   /**
-   * Save a message in the session. Uses $this->session->AddMessage()
-   *
-   * @param $type string the type of message, for example: notice, info, success, warning, error.
-   * @param $message string the message.
-   * @param $alternative string the message if the $type is set to false, defaults to null.
+   * Wrapper for same method in CAmvc.
    */
   protected function AddMessage($type, $message, $alternative=null) {
-    if($type === false) {
-      $type = 'error';
-      $message = $alternative;
-    } else if($type === true) {
-      $type = 'success';
-    }
-    $this->session->AddMessage($type, $message);
+    return $this->amvc->AddMessage($type, $message, $alternative);
   }
 
 
   /**
-   * Create an url. Uses $this->request->CreateUrl()
-   *
-   * @param $urlOrController string the relative url or the controller
-   * @param $method string the method to use, $url is then the controller or empty for current
-   * @param $arguments string the extra arguments to send to the method
+   * Wrapper for same method in CAmvc.
    */
   protected function CreateUrl($urlOrController=null, $method=null, $arguments=null) {
-    $this->request->CreateUrl($urlOrController, $method, $arguments);
+    return $this->amvc->CreateUrl($urlOrController, $method, $arguments);
   }
 
 }
